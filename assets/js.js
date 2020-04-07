5d78e5eeddc0cd2309df8ad8a948b43b96ec1c7a
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-04-07 16:19:40 -0600

    continued progress, added arrays and additional variables

--- assets/js.js
+++ assets/js.js
@@ -5,17 +5,33 @@ alert("We'll now begin.");
 //Variables Declared
     //Asks for a length of the new password -integer
 var length = prompt("How long should your new password be?");
+    //confirms the user wants capital letters included
+var capLetters = confirm("Include capital letters?");
     //confirms whether or not the password will contain special characters - boolean
 var specChar = confirm("Include special characters? (!@#$%^&:/?<> etc.)");
     //confirms whether or not the password will contain numbers
 var  Nums = confirm("Include numbers?");
-//var randNum = math.random
+    //var randNum = math.random
+var randNum;
 
+//Arrays containing possible characters for the password
+var letters = ["a","b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var capLets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var chars = ["!", "@", "#", "$", "%", "^", "&", "+", "=", "<", ">", "?", "/"];
 //functions
     //produces random number(s)
 function randomNum() {
 
 };
+
+function generate() {
+    if (specChar && Nums) {
+        while 
+    } else if {
+
+    }
+}
 
 
 
