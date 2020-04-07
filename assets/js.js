1dce1eaf67653b8a18f795b854684f46ae6e1e6d
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-04-06 23:14:23 -0600

    minor improvements

---
+++ assets/js.js
@@ -0,0 +1,28 @@
+//Welcome
+alert("Welcome!");
+alert("We'll now begin.");
+
+//Variables Declared
+    //Asks for a length of the new password -integer
+var length = prompt("How long should your new password be?");
+    //confirms whether or not the password will contain special characters - boolean
+var specChar = confirm("Include special characters? (!@#$%^&:/?<> etc.)");
+    //confirms whether or not the password will contain numbers
+var  Nums = confirm("Include numbers?");
+//var randNum = math.random
+
+//functions
+    //produces random number(s)
+function randomNum() {
+
+};
+
+
+
+//if statements to determine which while loop to run
+
+
+
+
+
+
