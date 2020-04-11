d88eb83b416495b0d5586cb94d8e0b3496acf3e9
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-04-10 21:51:41 -0600

    made more progress with the functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,27 @@
 //var - boolean values for checkbox password options
+var generateBtn = document.querySelector("#generate");
 var upper = document.getElementById('upper');
 var numbers = document.getElementById('numbers');
 var characters = document.getElementById('characters');
-var length = document.getElementById('length');
-var lenDisplay = document.getElementById('lenDisplay')
-    //*note to self: use ".checked" for boolean values*
-var value; //single value that is generated
-var result = ""; //array containing the single values for the password
+var lengthPass = document.getElementById('length');
 
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+    //math variables
+var index; //random number from the function, the index of the character
+
+var value; //single string value that is generated to be included in the password
+
+var result = []; //array containing the single values for the password. if array, .push(), if string, .concat()
 
 //arrays for generating values
 var lclet = ["a","b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+//26
 var upperC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"];
-
+//26
 var nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
+//10
 var specChar = ["!", "@", "#", "$", "%", "^", "&", "+", "=", "<", ">", "?", "/"];
+//13
 
 //array containing the functions used to generate values for the passwords. used to randomize whether the next value is a letter, number, or symbol.
 var funcs = [
@@ -28,43 +30,63 @@ var funcs = [
   randNumN(),
   randNumS()
 ];
-var using = [];
+var using = [randNumL()];
 
 //functions
 
+function usingFuncs() {
+  if (upper.checked) {
+    using.push(randNumU());
+  }; 
+
+  if (numbers.checked) {
+    using.push(randNumN());
+  };
+
+  if (characters.checked) {
+    using.push(randNumS());
+  };
+}
 //generates random number between 0-25, and the number is used as an index value for a lowercase letter.
 function randNumL() {
-  result = Math.floor(Math.random()*(25-0+1))+0;
+  index = Math.floor(Math.random()*(25-0+1))+0;
+  value = lclet[index];
+  result.push(value);
 };
 
 //generates random number between 0-25, and the number is used as an index value for a upper letter.
 function randNumU() {
-  result = Math.floor(Math.random()*(25-0+1))+0;
+  index = Math.floor(Math.random()*(25-0+1))+0;
+  value = upperC[index];
+  result.push(value);
 };
 
 //generates random number between 0-25, and the number is used as an index value for a number letter.
 function randNumN() {
-  result = Math.floor(Math.random()*(9-0+1))+0;
+  index = Math.floor(Math.random()*(9-0+1))+0;
+  value = nums[index];
+  result.push(value);
 };
 
 //generates random number between 0-25, and the number is used as an index value for a symbol.
 function randNumS() {
-  result = Math.floor(Math.random()*(12-0+1))+0;
+  index = Math.floor(Math.random()*(12-0+1))+0;
+  value = specChar[index];
+  result.push(value); 
 };
 
 //generates a random number, allowing the program to randomly generate a lower, upper, number, or special character.
-function randNumFuncs() {
-  result = Math.floor(Math.random()*(3-0+1))+0;
+function randFuncs(using) {
+  index = Math.floor(Math.random()*(((using.length) - 1)-0+1))+0;
+  using[index]();
 };
 
-
-function options() {
-
-};
 
 //generate the password
-function generatePassword(length) {
+function generatePassword(lengthPass) {
+  for (var i = 0; i < lengthPass; i++) {
 
+  }
 
 };
 
@@ -72,10 +94,10 @@ function generatePassword(length) {
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  result = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = result;
 };
 
 
@@ -83,11 +105,9 @@ function writePassword() {
 // Add event listener to generate button
 
 generateBtn.addEventListener("click", function() {
-  options(); 
+  usingFuncs(); 
   generatePassword();  
   writePassword()
-};
-
-
+});
 
 
